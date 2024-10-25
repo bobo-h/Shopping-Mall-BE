9867dcaf07503bdbbbc86ff82d1dbfaa6a364f75
bobo-h <166573095+bobo-h@users.noreply.github.com>
2024-10-25 11:58:01 +0900

    rename: change folder names from uppercase to lowercase

--- models/User.js
+++ models/User.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const userSchema = Schema(
   {
-    email: { type: String, required: true, unique: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
     password: { type: String, required: true },
     name: { type: String, required: true },
     level: { type: String, default: "customer" },
