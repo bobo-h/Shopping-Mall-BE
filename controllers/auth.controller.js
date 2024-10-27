b12adb773399190b0f65ccde77dbd8198d68c2bc
bobo-h <166573095+bobo-h@users.noreply.github.com>
2024-10-27 21:25:32 +0900

    feat: 상품 추가 로직

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -42,4 +42,17 @@ authController.authenticate = (req, res, next) => {
   }
 };
 
+authController.checkAdminPermission = async (req, res, next) => {
+  try {
+    const { userId } = req;
+    const user = await User.findById(userId);
+    if (user.level !== "admin") {
+      throw new Error("No permission");
+    }
+    next();
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
 module.exports = authController;
