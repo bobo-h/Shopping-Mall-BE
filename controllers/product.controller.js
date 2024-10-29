9479556bfb4be05af0c0e8fe4f18203563c815d4
bobo-h <166573095+bobo-h@users.noreply.github.com>
2024-10-29 23:39:03 +0900

    feat: add pagenation logic

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../models/Product");
 const productController = {};
 
+const PAGE_SIZE = 5;
 productController.createProduct = async (req, res) => {
   try {
     const {
@@ -38,10 +39,18 @@ productController.getProducts = async (req, res) => {
     // 정규화로 키워드가 포함인 상품까지 = regex / 대소문자 구분 없이 = options:"i"
     const cond = name ? { name: { $regex: name, $options: "i" } } : {};
     let query = Product.find(cond);
-
+    let response = { status: "success" };
+    if (page) {
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      // 데이터 총 몇개인지 = 총 개수 / PAGE_SIZE
+      const totalItemNum = await Product.find(cond).count();
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
     // query를 실행시키고 싶을 때 하겠다는 것 = exec()
     const productList = await query.exec();
-    res.status(200).json({ status: "success", data: productList });
+    response.data = productList;
+    res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
