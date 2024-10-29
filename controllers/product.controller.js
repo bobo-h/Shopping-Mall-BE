0a502bdb0a5b01e2fc7f1b9fc31f4c8a95b3ac77
bobo-h <166573095+bobo-h@users.noreply.github.com>
2024-10-29 23:06:15 +0900

    fix: options

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -35,8 +35,8 @@ productController.createProduct = async (req, res) => {
 productController.getProducts = async (req, res) => {
   try {
     const { page, name } = req.query;
-    // 정규화로 키워드가 포함인 상품까지 = regex / 대소문자 구분 없이 = option:"i"
-    const cond = name ? { name: { $regex: name, $option: "i" } } : {};
+    // 정규화로 키워드가 포함인 상품까지 = regex / 대소문자 구분 없이 = options:"i"
+    const cond = name ? { name: { $regex: name, $options: "i" } } : {};
     let query = Product.find(cond);
 
     // query를 실행시키고 싶을 때 하겠다는 것 = exec()
