cfc564b39fcda7a47e79899542fe8f1528efd7aa
bobo-h <166573095+bobo-h@users.noreply.github.com>
2024-11-09 18:48:06 +0900

    feat: 구글 로그인 로직 생성

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,10 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const { GoogleAuth } = require("google-auth-library");
 require("dotenv").config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 
 const authController = {};
 
@@ -20,6 +22,46 @@ authController.loginWithEmail = async (req, res) => {
     throw new Error("이메일 또는 비밀번호를 다시 확인해주세요");
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+authController.loginWithGoogle = async (req, res) => {
+  try {
+    // 백엔드에서 로그인하기
+    // 토큰값을 읽어와서 => 유저정보를 뽑아내고 email
+    // a. 이미 로그인을 한적이 있는 유저 => 로그인 시키고 토큰값 준다
+    // b. 처음 로그인 시도를 한 유저 => 유저 정보 먼저 새로 생성하고 토큰 값을 준다.
+    const { token } = req.body;
+    const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
+
+    // 구글에서 만든 토큰이 맞는지 확인
+    const ticket = await googleClient.verifyIdToken({
+      idToken: token,
+      audience: GOOGLE_CLIENT_ID,
+    });
+
+    const { email, name } = ticket.getPayload();
+    console.log("eee", email, name);
+
+    // 유저가 존재하는지 찾기
+    let user = await User.findOne({ email });
+    if (!user) {
+      // 없다 -> 유저를 새로 생성
+      const randomPassword = "" + Math.floor(Math.random() * 10000000);
+      const salt = await bcrypt.genSalt(10);
+      const newPassword = await bcrypt.hash(randomPassword, salt);
+      user = new User({
+        name,
+        email,
+        password: newPassword,
+      });
+      await user.save();
+    }
+    // 토큰발행 리턴
+    const sessionToken = await user.generateToken();
+    res.status(200).json({ status: "success", user, token: sessionToken });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
   }
 };
 
